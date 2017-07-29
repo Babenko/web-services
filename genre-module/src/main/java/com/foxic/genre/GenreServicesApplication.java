e3b5cf88c8536be81237232b29caa82fe24d11e7
Babenko <babenkosemen@gmail.com>
2017-07-30 01:59:46 +0300

    Initial commit

---
+++ genre-module/src/main/java/com/foxic/genre/GenreServicesApplication.java
@@ -0,0 +1,12 @@
+package com.foxic.genre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenreServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenreServicesApplication.class, args);
+	}
+}
