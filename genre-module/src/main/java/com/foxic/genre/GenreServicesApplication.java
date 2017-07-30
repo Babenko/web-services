a2ee0b44b74b2b4488e7b1a08bd7a2e9aa47806d
Babenko <babenkosemen@gmail.com>
2017-07-30 22:29:14 +0300

    Add config

--- genre-module/src/main/java/com/foxic/genre/GenreServicesApplication.java
+++ genre-module/src/main/java/com/foxic/genre/GenreServicesApplication.java
@@ -1,12 +1,20 @@
 package com.foxic.genre;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GenreServicesApplication {
+public class GenreServicesApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GenreServicesApplication.class, args);
+	}
+
+
+
+	@Override
+	public void run(String... strings) throws Exception {
+		System.out.println("fdsf");
 	}
 }
