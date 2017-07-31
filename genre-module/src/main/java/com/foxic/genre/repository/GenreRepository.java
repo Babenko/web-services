964da09f95a28cdf7a1501477ccb58e82230ff41
Babenko <babenkosemen@gmail.com>
2017-07-31 22:12:23 +0300

    Db settings

---
+++ genre-module/src/main/java/com/foxic/genre/repository/GenreRepository.java
@@ -0,0 +1,16 @@
+package com.foxic.genre.repository;
+
+import com.foxic.genre.dto.Genre;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Sem Babenko.
+ */
+@Repository
+public class GenreRepository {
+
+
+  public Genre findAll() {
+
+  }
+}
