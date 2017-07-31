acece41acfeb35fc0a5976660a2e1962b6a3e666
Babenko <babenkosemen@gmail.com>
2017-07-31 23:18:08 +0300

    Minor refactoring

--- genre-module/src/main/java/com/foxic/genre/GenreServicesApplication.java
+++ genre-module/src/main/java/com/foxic/genre/GenreServicesApplication.java
@@ -1,20 +1,12 @@
 package com.foxic.genre;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GenreServicesApplication implements CommandLineRunner {
+public class GenreServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GenreServicesApplication.class, args);
-	}
-
-
-
-	@Override
-	public void run(String... strings) throws Exception {
-		System.out.println("fdsf");
 	}
 }
--- genre-module/src/main/java/com/foxic/genre/repository/GenreRepository.java
+++ genre-module/src/main/java/com/foxic/genre/repository/GenreRepository.java
@@ -1,6 +1,9 @@
 package com.foxic.genre.repository;
 
 import com.foxic.genre.dto.Genre;
+import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -18,6 +21,15 @@ public class GenreRepository {
   }
 
   public List<Genre> findAll() {
-    return jdbcTemplate.query("select * from genre", (rs, rowNum) -> new Genre());
+    return jdbcTemplate.query("select * from genre", (rs, rowNum) -> populateGenre(rs));
+  }
+
+  private Genre populateGenre(ResultSet resultSet) throws SQLException {
+    Genre genre = new Genre();
+    genre.setId(BigInteger.valueOf(resultSet.getLong("id")));
+    genre.setMovieId(BigInteger.valueOf(resultSet.getLong("movieId")));
+    genre.setDescription(resultSet.getString("description"));
+    genre.setName(resultSet.getString("name"));
+    return genre;
   }
 }
