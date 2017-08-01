dd26c0f1381b88377ef8c0cdcd5f8607dd6fde25
Babenko <babenkosemen@gmail.com>
2017-08-01 23:33:10 +0300

    Additional data

--- genre-module/src/main/resources/db/migration/V2__populate_genre_table.sql
+++ genre-module/src/main/resources/db/migration/V2__populate_genre_table.sql
@@ -1,1 +1,3 @@
 insert into GENRE (movieId, name, description) values (1, 'Action', 'some description about action');
+insert into GENRE (movieId, name, description) values (2, 'Drama', 'some description about drama');
+insert into GENRE (movieId, name, description) values (2, 'Adventure', 'some description about adventure');
