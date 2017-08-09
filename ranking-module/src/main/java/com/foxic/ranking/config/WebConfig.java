a8c2a279af82f0aa9f5620e4e436ee350c2b88cc
Babenko <babenkosemen@gmail.com>
2017-08-09 23:14:00 +0300

    Prepare endpoint bean

--- ranking-module/src/main/java/com/foxic/ranking/config/WebConfig.java
+++ ranking-module/src/main/java/com/foxic/ranking/config/WebConfig.java
@@ -1,9 +1,17 @@
 package com.foxic.ranking.config;
 
+import javax.xml.ws.Endpoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by Sem Babenko.
  */
-
+@Configuration
 public class WebConfig {
 
+  @Bean
+  public Endpoint endpoint() {
+    return null;
+  }
 }
