523478a754db787c1946077c4ea4d17e3d3fc744
Babenko <babenkosemen@gmail.com>
2017-08-13 15:17:23 +0300

    Migration

---
+++ ranking-module/src/main/resources/db/migration/V2__populate_ranking_table.sql
@@ -0,0 +1,3 @@
+insert into RANKING (movieId, mark, description) values (1, 7, 'some description about 7 mark');
+insert into RANKING (movieId, mark, description) values (2, 2, 'some description about 2 mark');
+insert into RANKING (movieId, mark, description) values (2, 3, 'some description about 3 mark');
