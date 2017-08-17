ec2441902670742e3303f599e6944c502c7d23ae
Babenko <babenkosemen@gmail.com>
2017-08-17 23:07:40 +0300

    Mark dto and repository

---
+++ ranking-module/src/main/java/com/foxic/ranking/dto/Rank.java
@@ -0,0 +1,15 @@
+package com.foxic.ranking.dto;
+
+import lombok.Data;
+
+/**
+ * Created by Sem Babenko.
+ */
+@Data
+public class Rank {
+
+  private Integer id;
+  private Integer moveId;
+  private Integer mark;
+  private String description;
+}
--- ranking-module/src/main/java/com/foxic/ranking/repository/RankingRepository.java
+++ ranking-module/src/main/java/com/foxic/ranking/repository/RankingRepository.java
@@ -1,5 +1,10 @@
 package com.foxic.ranking.repository;
 
+import com.foxic.ranking.dto.Rank;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -8,5 +13,23 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class RankingRepository {
 
+  private JdbcTemplate jdbcTemplate;
+
+  public RankingRepository(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  public List<Rank> loadAll() throws Exception {
+    return jdbcTemplate.query("SELECT * FROM ranking", (row, index) -> populateRank(row));
+  }
+
+  private Rank populateRank(ResultSet row) throws SQLException {
+    Rank rank = new Rank();
+    rank.setId(row.getInt("id"));
+    rank.setMoveId(row.getInt("moveId"));
+    rank.setMark(row.getInt("mark"));
+    rank.setDescription(row.getString("description"));
+    return rank;
+  }
 
 }
